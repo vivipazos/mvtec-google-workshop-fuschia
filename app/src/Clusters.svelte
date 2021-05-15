994ad738c7b18c42285edddfee86a487fcf8cc95
Matt Osborn <mattjosborn@gmailcom>
2021-05-15 17:11:47 +0100

    cluster annotations

--- app/src/Clusters.svelte
+++ app/src/Clusters.svelte
@@ -34,16 +34,23 @@ const clusters = {
 };
 
 let width = 1000;
-let height = 600;
+let height = 500;
 
 let data = new Array(100).fill()
 	.map(a => ({
 		x: Math.random()*width,
 		y: Math.random()*height,
+        r: 10
 	}));
 
 const randomSimulation = forceSimulation(data)
-    .on("tick", () => data = data)
+    .on("tick", () => {
+        data.forEach(d => {
+            d.x = Math.max(10, Math.min(width-10, d.x));
+            d.y = Math.max(10, Math.min(height-10, d.y));
+        })
+        data = data;
+    })
 
 const randomForce = function() {
     const str = 0.5;
@@ -64,29 +71,34 @@ const randomForce = function() {
 }
 
 window.setInterval(() => {
-    randomSimulation.force('random', randomForce())
-    randomSimulation.alpha(1);
-    randomSimulation.restart();
+    randomSimulation
+        .force('random', randomForce())
+        .force("collide", forceCollide(d => d.r + 2))
+        .alpha(1)
+        .restart();
 }, 500)
 
 var prevComponent;
 var clusterSimulation;
+var annotations;
+
 $: {
     if (prevComponent !== selectedObject) {
-        let offset = 0
-        Object.keys(selectedObject)
+        let offset = 0;
+        const clusterNames = Object.keys(selectedObject)
             .filter(key => key !== 'month')
-            .sort()
-            .forEach(key => {
-                for (var i = 0; i < selectedObject[key]; i++) {
-                    var d = data[offset+i];
-                    d.x0 = clusters[key].x0
-                    d.y0 = clusters[key].y0
-                    d.color = clusters[key].color;
-                    d.key = key;
-                }
-                offset += selectedObject[key]
-            })
+            .sort();
+
+        clusterNames.forEach(key => {
+            for (var i = 0; i < selectedObject[key]; i++) {
+                var d = data[offset+i];
+                d.x0 = clusters[key].x0
+                d.y0 = clusters[key].y0
+                d.color = clusters[key].color;
+                d.key = key;
+            }
+            offset += selectedObject[key]
+        })
 
 
         const clusteredData = data.filter(d => d.key !== "Other")
@@ -94,10 +106,39 @@ $: {
         if (clusterSimulation) { clusterSimulation.stop(); }
 
         clusterSimulation = forceSimulation(clusteredData)
-            .force("x",forceX(d => d.x0 === null ? d.x : d.x0))
-            .force("y",forceY(d => d.y0 === null ? d.y : d.y0))
-            .force("collide", forceCollide(12))
+            .alphaDecay(0.0228 / 6)
+            .velocityDecay(0.3)
+            .force("x",forceX(d => d.x0 === null ? d.x : d.x0).strength(0.01))
+            .force("y",forceY(d => d.y0 === null ? d.y : d.y0).strength(0.01))
+            .force("collide", forceCollide(d => d.r + 2))
             .on("tick", () => data = data)
+
+        annotations = clusterNames
+            .filter(key => selectedObject[key] > 0 && key !== "Other")
+            .map(key => {
+                const c = clusters[key];
+                return {
+                    name: key,
+                    x: c.x0,
+                    y: c.y0,
+                    r: Math.sqrt(selectedObject[key] * 10) * 5
+                }
+            });
+
+        const antiCluster = clusterNames.map(key => {
+            const c = clusters[key];
+            return {
+                fx: c.x0,
+                fy: c.y0,
+                r: Math.sqrt(selectedObject[key] * 10) * 5
+            }
+        })
+
+        console.log(antiCluster);
+
+        const randomNodes = data.filter(d => d.key === "Other")
+        randomSimulation
+            .nodes([...randomNodes, ...antiCluster])
     }
     prevComponent = selectedObject;
 }
@@ -108,11 +149,25 @@ $: {
 <main>
     <svg viewBox="0 0 {width} {height}">
         {#each data as d}
-        <circle cx={d.x} cy={d.y} r="10" fill={d.color}/>
+        <circle cx={d.x} cy={d.y} r={d.r} fill={d.color}/>
+        {/each}
+        {#each annotations as d}
+        <circle cx={d.x} cy={d.y} r={d.r} fill="none" stroke="#333" data-name={d.name}/>
+        <text x={d.x} y={d.y - d.r - 4}>
+            {d.name}
+        </text>
         {/each}
     </svg>
 </main>
 
 <style>
+
+    svg {
+        border: 1px solid black;
+    }
+
+    text {
+        text-anchor: middle;
+    }
 
 </style>
