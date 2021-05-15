5965c36c7febf13941a2662332b4740927547820
Víctor <victorgd97@gmail.com>
2021-05-15 18:45:04 +0200

    sections

--- app/src/App.svelte
+++ app/src/App.svelte
@@ -28,9 +28,51 @@
 		</div>
 
 		<div slot="foreground">
-		  <section>This is the first section.</section>
-		  <section>This is the second section.</section>
-		  <section>This is the third section.</section>
+			<section>We all want to ✨help✨. <br>
+				But, according to Google search, not for <br> long enough. <br> <br> <br>
+				Let’s reimagine the Google search index as 100 people,<br> represented by these floating circles.
+			</section>
+
+			<section>
+				<h>DECEMBER  2019</h>
+				We start our story in December 2019. Just before the pandemic hits. 
+				Three of us cared about fires burning in Australia.
+			</section>
+		
+			<section>
+				<h>JANUARY 2020</h>
+				Interest in helping for the Australian bushfires peaked in January, while interest in Coronavirus was just beginning. 
+			</section>
+
+			<section>
+				<h>FEBRUARY 2020</h>
+				In less than a month, everyone’s attention was elsewhere. Just one person was left caring about the bushfires while Coronavirus interest rose to five people. 
+			</section>
+
+			<section>
+				<h>MARCH 2020</h>
+				At the peak of the first wave, almost everyone was trying to search ways to help with Coronavirus. 
+			</section>
+
+			<section>
+				<h>APRIL 2020</h>
+				Slowly, people drifted away. A month later, the number of people searching how to help with Coronavirus dropped by a third.
+			</section>
+
+			<section>
+				<h>MAY 2020</h>
+				Just two months after its peak, interest in Coronavirus dropped to just 16 people. Black Lives Matter protests were on the rise.
+			</section>
+
+			<section>
+				<h>JUNE 2020</h>
+				The death of George Floyd sparks wide interest in the Black Lives Matter protest. Yemen’s crisis from Coronavirus also gets some interest.
+			</section>
+
+			<section>
+				<h>JULY 2020</h>
+				Less than a fifth of people are still searching how to help, falling dramatically in just a month. And, looking back at all the data since 2015, this is a common trend for crisis events.
+			</section>
 		</div>
 	  </Scroller>
 	<!-- <Test /> -->
@@ -89,5 +131,6 @@
 	}
 	section {
 		height: 80vh;
+		text-align: center;
 	}
 </style>
