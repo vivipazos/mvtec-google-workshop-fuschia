d5db066b339002401273d3365849acf4f3a25324
rvpazos <rvpazos@sph.com.sg>
2021-05-15 11:15:31 +0800

    playing

--- app/src/App.svelte
+++ app/src/App.svelte
@@ -2,22 +2,27 @@
 	import Menu from './components/common/Menu.svelte'
 	import Test from './test/Test.svelte'
 	import Footer from './components/common/Footer.svelte'
+	// import Random from './components/common/Random.svelte'
 
 </script>
 
 <main>
+	<h1>We all want to <strong>help</strong>.</h1>
+	<h2>But, according to Google search, not for <strong>long enough</strong>.</h2>
+	<p>Let's reimagine the Google search index as 100 people, represented by these floating circles.</p>
+	<!-- <Test /> -->
 
-	<Test />
+	<!-- <Random /> -->
 
 	<Footer>
 		<div slot="about">
-			I could not but remark the striking difference of his attention, and that of Lord Orville: the latter has such gentleness of manners, such delicacy of conduct, and an air so respectful, that, when he flatters most, he never distresses; and when he most confers honour, appears to receive it! The former obtrudes his attention, and forces mine; it is so pointed, that it always confuses me, and so public, that it attracts general notice. Indeed I have sometimes thought that he would rather wish, than dislike to have his partiality for me known, as he takes great care to prevent my being spoken to by any but himself.
+			Google search trends reveal a lot about what it... 
 		</div>
 		<div slot="data">
-			He bowed, expecting me to go;-and then, with quick steps, but a heavy heart, I came to my own room. His question, I am sure, meant Sir Clement Willoughby; and had I not imposed upon myself the severe task of avoiding, flying Lord Orville, with all my power, I would instantly have satisfied him of my ignorance of Sir Clement's journey. And yet more did I long to say something of the assembly, since I found he depended upon my spending the evening at home.
+			The data is indexed to...
 		</div>
 		<div slot="team">
-			Student, student, student ...
+			Rebecca Pazos, Rocío Márquez, Victor and Oriol.
 		</div>
 	</Footer>
 </main>
@@ -28,4 +33,12 @@
 		padding-bottom: 0;
 		margin: 0 auto;
 	}
+
+	h1, h2, p {
+		max-width: 500px;
+		margin: 0 auto;
+		text-align: center;
+		padding: 50px;
+	}
+
 </style>
--- app/src/components/common/Footer.svelte
+++ app/src/components/common/Footer.svelte
@@ -5,6 +5,7 @@
     import UdG from '../../img/UdG.svelte'
     import Tweet from './Tweet.svelte'
 </script>
+
 <div class='fs footer'>
     <div class='col'>
     <Container>
@@ -27,7 +28,7 @@
                 <h4>Made by</h4>
                 <p><slot name='team'></slot></p>
                 <p><b>Google News Lab:</b> <a href='https://simonrogers.net/'>Simon Rogers</a> and <a href='http://albertocairo.com/'>Alberto Cairo</a></p>
-                <p><b>MVTEC mentors: </b> Mentor, mentor, and mentor</p>
+                <p><b>MVTEC mentors: </b> <a href="https://www.karmapeiro.com/en/landing-en/">Karma Peiró</a> and Matt Osborn.</p>
             </Col>
         </Row>
         <p>This project was designed and developed in the <a href='http://www.mastervisualtoolsudg.com/'>Master’s program in Visual Tools to Empower Citizens (MVTEC)</a> organized by <a href='https://www.fundacioudg.org/'>Fundació UdG</a> and the <a href='https://www.fundaciovit.org/'>ViT Foundation</a></p>
